2c0f40ef0e380bd2c589fc1350ddb311fca93451
Solomon <sveidling@gmail.com>
2023-09-10 17:46:01 +0300

    commented my code

--- api/app/main.py
+++ api/app/main.py
@@ -6,27 +6,26 @@ from io import BytesIO
 from PIL import Image
 
 app = FastAPI()
-model = torch.hub.load(
+
+model = torch.hub.load( # Загружаем тренированную модель
     'yolo',
     'custom',
-    path='api/utils/weights.pt',
+    path='api/utils/weights.pt', 
     source='local'
 )
 
-@app.get('/')
+@app.get('/') # 0.0.0.0:8000/
 def return_info():
-    return 'Based on FastAPI'
+    return 'Based on FastAPI' 
 
-@app.post('/detect')
-def detect(file: UploadFile = File(...)):
+@app.post('/detect') # 0.0.0.0:8000/detect - адрес по которому мы будем в стримлит-файле
+def detect(file: UploadFile = File(...)):  # отправлять post-запрос и получать ответ
 
-    image_buffer = file.file.read()
-    print(image_buffer)
-    source_image = Image.open(BytesIO(image_buffer))
-    print(type(source_image))
-    results = model(source_image)
+    image_buffer = file.file.read()                  # получаем файл
+    source_image = Image.open(BytesIO(image_buffer)) # читаем его содеражние (байтсы) 
+    results = model(source_image)                    
     df = results.pandas().xyxy[0]
-    bboxNlabel = []
+    bboxNlabel = []                                  # создаем список для координат bbox'сов и class label'лов
     for index, row in df.iterrows():
         row = row.tolist()
         xA = int(row[0])
@@ -35,5 +34,5 @@ def detect(file: UploadFile = File(...)):
         yB = int(row[3])
         label = row[6]
         bboxNlabel.append((xA, yA, xB, yB, label))
-    bboxNlabel = jsonable_encoder({"data": bboxNlabel})
-    return JSONResponse(content=bboxNlabel)
+    bboxNlabel = jsonable_encoder({"data": bboxNlabel}) # кодируем в json словарь
+    return JSONResponse(content=bboxNlabel)             # отправляем в streamlit
--- front/main.py
+++ front/main.py
@@ -14,22 +14,21 @@ def main():
     source_image = st.file_uploader("Upload an image")
     image_url = st.text_input("##### Or pass URL", "")
 
-    if source_image is not None:  # Проверка, загружено ли изображение
+    if source_image is not None:                                # Проверка, загружено ли изображение
         source_image = Image.open(source_image)
         source_image = source_image.convert('RGB') 
-    elif image_url != "":  # Проверка, был ли введен URL
-        # Download the image from the URL
-        response = requests.get(image_url)
-        source_image = Image.open(BytesIO(response.content))
+    elif image_url != "":                                       # Проверка, был ли введен URL
+        response = requests.get(image_url)                      # Cкачиваем изображение (байтсы)
+        source_image = Image.open(BytesIO(response.content))    # Читаем его
         source_image = source_image.convert('RGB')
     else:
-        st.write("### You haven't provided us with a picture yet")
+        st.markdown("<h3 style='color: red;'>You haven't provided us with a picture yet</h3>", unsafe_allow_html=True)
 
     col1, col2 = st.columns(2)
     if source_image:
         with col1:
             try:
-                st.image(source_image,
+                st.image(source_image,                          # Отображаем исходное изображение
                         caption="Source",
                         )
             except Exception as ex:
@@ -37,21 +36,17 @@ def main():
                     f"Upload a picture")
         with col2:
             try:
-                # Convert PIL image to BytesIO
-                image_buffer = BytesIO()
-                source_image.save(image_buffer, format='JPEG')
-                # Reset the buffer's file pointer
-                image_buffer.seek(0)
-                # Create the files dictionary with the image buffer
-                file = {"file": image_buffer}
-                response = requests.post("http://0.0.0.0:8000/detect", files=file)
-                bboxNlabel = json.loads(response.text)['data']
-                print(bboxNlabel)
+                image_buffer = BytesIO()                          # Создаем объект BytesIO
+                source_image.save(image_buffer, format='JPEG')    # Convert PIL image to BytesIO
+                image_buffer.seek(0)                              # Сбрасываем файл пойнтер в буфере. Когда мы записываем файл, он оказывается в конце.
+                file = {"file": image_buffer}                     # Создаем словарь для отправки в апи
+                response = requests.post("http://0.0.0.0:8000/detect", files=file)  
+                bboxNlabel = json.loads(response.text)['data']    # Читаем json словарь с ответом
 
-                source_image = np.array(source_image)
+                source_image = np.array(source_image)             # cv2 работает с np array
                 for x in bboxNlabel:
-                    image = cv2.rectangle(source_image, x[0:2], x[2:4], (0, 255, 0), 2)
-                    for lst in bboxNlabel:
+                    image = cv2.rectangle(source_image, x[0:2], x[2:4], (0, 255, 0), 2) # Рисуем рамки зеленого цвета
+                    for lst in bboxNlabel:                                              # Делаем подпись с классом
                         image = cv2.putText(image, lst[4], (lst[0], lst[1]-7), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
                 st.image(image,
                         caption="Detection",
